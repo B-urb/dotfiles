5381a0dd620fb0f9735e5aad092eed8f03b79f80
Björn Urban <bjoclurban@gmail.com>
2022-07-05 17:49:15 +0200

    Readd kube config helper script

--- kube/clusters/merge_clusters.sh
+++ kube/clusters/merge_clusters.sh
@@ -1,2 +1,15 @@
-flflf:wq
-
+#!/bin/bash
+CONFIGS=""
+FILES="./*"
+SEPARATOR=":"
+for f in $FILES
+do
+	if [ $f != "./merge_clusters.sh" ]
+	then
+		echo $f
+		CONFIGS=$CONFIGS$SEPARATOR$f
+	fi
+done
+echo $CONFIGS
+KUBECONFIG=$CONFIGS kubectl config view \
+    --merge --flatten > ../config
