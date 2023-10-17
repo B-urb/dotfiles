1181841eaab95bbcff7d1851f68342e7a9afe6a5
Björn Urban <burban@inovex.de>
2023-10-17 16:48:41 +0200

    updated sketchybar and added Brewfile

--- config/sketchybar/plugins/wifi.sh
+++ config/sketchybar/plugins/wifi.sh
@@ -1,9 +1,28 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
-SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
-AUTH="$(echo "$CURRENT_WIFI" | grep -o "link auth: .*" | sed 's/^link auth: //')"
-CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
-MAX_TX="$(echo "$CURRENT_WIFI" | grep -o "maxRate: .*" | sed 's/^maxRate: //')"
+update() {
+  source "$CONFIG_DIR/icons.sh"
+  INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
+  LABEL="$INFO ($(ipconfig getifaddr en0))"
+  ICON="$([ -n "$INFO" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
 
-sketchybar --set $NAME icon="$SSID:" label="$CURR_TX / $MAX_TX (Mbps) - $AUTH"
+  sketchybar --set $NAME icon="$ICON" label="$LABEL"
+}
+
+click() {
+  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
+
+  WIDTH=0
+  if [ "$CURRENT_WIDTH" -eq "0" ]; then
+    WIDTH=dynamic
+  fi
+
+  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
+}
+
+case "$SENDER" in
+  "wifi_change") update
+  ;;
+  "mouse.clicked") click
+  ;;
+esac
